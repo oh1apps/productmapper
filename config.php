ee395a0ecc353521b009c7616d12100b099a2bcd
gk <komorovg@gmail.com>
2025-01-28 00:07:53 +0100

    update mapper.php and postal.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$mapper_password = "admin"; // Hasło do panelu mappera produktów
+$shop_id = 0; // ID sklepu Shoper (znajdziesz w adresie URL, przchodząc do Integracje > Sklepy > Edytuj)
+
+$sa_panel_url = "https://example.sellasist.pl"; // Adres panelu Sellasist
+$sa_api_url = "https://example.sellasist.pl/api/v1/"; // Adres API Sellasist
+$sa_api_key = ""; // Klucz API Sellasist
